24841f2734ad437713c2518b03fadde2f4e0a2b5
PhyaeSoneThwim <pyaesonethwim1998@gmail.com>
2020-08-23 18:08:17 +0630

    apply express middlewares and prepare folder and file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 require("dotenv").config({});
+const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
 const dbConnect = require("./utils/dbConnect");
+const globalError = require("./middlewares/error");
 const app = express();
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "failed",
+    message: "no route defined",
+  });
+});
+
+//setup global error handler
+app.use(globalError);
 //setup database connection
 dbConnect();
 
